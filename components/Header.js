ac645cef42febc31e15fd673cdbe85518371e9d9
lvidal1 <lvidal910@gmail.com>
2022-11-16 23:58:11 -0500

    [UI] Remove unused tags

--- components/Header.js
+++ components/Header.js
@@ -3,12 +3,8 @@ import NavItem from "./NavItem";
 const Header = () => {
     return <header>
         <nav className="font-sans flex flex-row text-left justify-between py-4 px-6 bg-white shadow items-baseline w-full">
-            <div className="mb-2 sm:mb-0">
-                <NavItem url="/home" >Home</NavItem>
-            </div>
-            <div>
-                <NavItem url="/sandbox">Sandbox</NavItem>
-            </div>
+            <NavItem url="/">Home</NavItem>
+            <NavItem url="/sandbox">Sandbox</NavItem>
         </nav>
     </header>;
 }
