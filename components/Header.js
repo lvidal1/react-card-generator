ad946c687d050a78e15afef61d18352ce3fcf65c
lvidal1 <lvidal910@gmail.com>
2022-11-16 23:55:58 -0500

    [feature/header] Add basic nav items

--- components/Header.js
+++ components/Header.js
@@ -1,6 +1,15 @@
+import NavItem from "./NavItem";
+
 const Header = () => {
     return <header>
-        <h1>Header</h1>
+        <nav className="font-sans flex flex-row text-left justify-between py-4 px-6 bg-white shadow items-baseline w-full">
+            <div className="mb-2 sm:mb-0">
+                <NavItem url="/home" >Home</NavItem>
+            </div>
+            <div>
+                <NavItem url="/sandbox">Sandbox</NavItem>
+            </div>
+        </nav>
     </header>;
 }
 
---
+++ components/NavItem.js
@@ -0,0 +1,5 @@
+const NavItem = ({ url, children }) => {
+    return <a href={url} className="text-lg no-underline text-grey-darkest hover:text-blue-dark">{children}</a>;
+}
+
+export default NavItem;
