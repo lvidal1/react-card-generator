0a93d4d8b03baba79d39de94ac920a74cdbe5845
lvidal1 <lvidal910@gmail.com>
2022-11-17 20:46:35 -0500

    [Config] Add jest & js properties

--- jest.config.js
+++ jest.config.js
@@ -15,6 +15,13 @@ const customJestConfig = {
     moduleDirectories: ['node_modules', '<rootDir>/'],
     testEnvironment: 'jest-environment-jsdom',
     setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
+    moduleNameMapper: {
+        '^@components/(.*)$': '<rootDir>/components/$1',
+        '^@pages/(.*)$': '<rootDir>/pages/$1',
+        '^@styles/(.*)$': '<rootDir>/styles/$1',
+
+
+    },
 }
 
 // createJestConfig is exported this way to ensure that next/jest can load the Next.js config which is async
