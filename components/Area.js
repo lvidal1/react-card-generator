0e0fb58ab9f9028a5ad25b3b9d4624d572869626
lvidal1 <lvidal910@gmail.com>
2022-11-18 00:44:01 -0500

    [Lint] Run prettier

--- components/Area.js
+++ components/Area.js
@@ -1,12 +1,13 @@
 import React from "react";
 
-import styles from "@styles/Area.module.css"
+import styles from "@styles/Area.module.css";
 
 const Area = () => {
-    return <div className={styles.container}>
-        <div className={styles.layer} id="layer" data-testid="Area.layer">
-        </div>
-    </div>;
-}
+	return (
+		<div className={styles.container}>
+			<div className={styles.layer} id="layer" data-testid="Area.layer"></div>
+		</div>
+	);
+};
 
 export default Area;
