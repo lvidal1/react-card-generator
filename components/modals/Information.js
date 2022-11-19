c3905852ad0a033288e62004ed9bbc5cf468ad21
lvidal1 <lvidal910@gmail.com>
2022-11-19 00:57:32 -0500

    [UI] Add field state for modal

--- components/modals/Information.js
+++ components/modals/Information.js
@@ -1,22 +1,40 @@
-import React from "react";
+import React, { useState } from "react";
 
 import Modal from "@components/shared/Modal";
 import Input from "@components/shared/Input";
-import Button from "../shared/Button";
+import Button from "@components/shared/Button";
 
 const Information = () => {
-	return (
-		<Modal>
-			<Modal.Header>Add your personal info</Modal.Header>
-			<Modal.Body>
-				<Input id="Modal.Information.Fullname" label="Full name" />
-				<Input id="Modal.Information.Birth" label="Birth day" />
-			</Modal.Body>
-			<Modal.Footer>
-				<Button text="Save" onClick={() => {}} />
-			</Modal.Footer>
-		</Modal>
-	);
+
+    const [fullName, setFullName] = useState("");
+    const [birth, setBirth] = useState("");
+
+    const onSubmit = () => {
+        if (fullName.length && birth.length) {
+            console.log(fullName, birth)
+        }
+    }
+
+    function handleFullNameChange(e) {
+        setFullName(e.target.value);
+    }
+
+    function handleBirthChange(e) {
+        setBirth(e.target.value);
+    }
+
+    return (
+        <Modal>
+            <Modal.Header>Add your personal info</Modal.Header>
+            <Modal.Body>
+                <Input id="Modal.Information.Fullname" label="Full name" value={fullName} onChange={handleFullNameChange} />
+                <Input id="Modal.Information.Birth" label="Birth day" value={birth} onChange={handleBirthChange} />
+            </Modal.Body>
+            <Modal.Footer>
+                <Button text="Save" onClick={onSubmit} />
+            </Modal.Footer>
+        </Modal>
+    );
 };
 
 export default Information;
