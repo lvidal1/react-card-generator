20405f8ea8222bb30bb8821cca9eab1cee9a84bf
lvidal1 <lvidal910@gmail.com>
2022-11-20 23:59:22 -0500

    [Lint] Run prettier

--- components/modals/Country.js
+++ components/modals/Country.js
@@ -4,15 +4,15 @@ import Modal from "@components/shared/Modal";
 import Button from "@components/shared/Button";
 
 const Country = () => {
-    return <Modal>
-        <Modal.Header >Set your country</Modal.Header>
-        <Modal.Body>
-
-        </Modal.Body>
-        <Modal.Footer>
-            <Button text="Save" />
-        </Modal.Footer>
-    </Modal>;
-}
+	return (
+		<Modal>
+			<Modal.Header>Set your country</Modal.Header>
+			<Modal.Body></Modal.Body>
+			<Modal.Footer>
+				<Button text="Save" />
+			</Modal.Footer>
+		</Modal>
+	);
+};
 
 export default Country;
