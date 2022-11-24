c20672c6e61a0c8e047d60813f56fadc565db712
lvidal1 <lvidal910@gmail.com>
2022-11-23 23:31:11 -0500

    [Store] Add technology slice to store

--- store/index.js
+++ store/index.js
@@ -3,6 +3,7 @@ import userReducer from "./slices/user";
 import modalReducer from "./slices/modal";
 import positionReducer from "./slices/position";
 import countryReducer from "./slices/country";
+import technologyReducer from "./slices/technology";
 
 export default configureStore({
 	reducer: {
@@ -10,5 +11,6 @@ export default configureStore({
 		modal: modalReducer,
 		position: positionReducer,
 		country: countryReducer,
+		technology: technologyReducer,
 	},
 });
