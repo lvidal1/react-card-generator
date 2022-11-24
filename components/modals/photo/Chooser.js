fddcfff89a1217e63f4f7c52b274ce857eece368
lvidal1 <lvidal910@gmail.com>
2022-11-24 17:56:04 -0500

    Updated button component & added ctas

--- components/modals/photo/Chooser.js
+++ components/modals/photo/Chooser.js
@@ -3,6 +3,7 @@ import { useDispatch } from "react-redux";
 import { togglePhotoChooser } from "@store/slices/modal";
 
 import Modal from "@components/shared/Modal";
+import Button from "@components/shared/Button";
 
 const Chooser = () => {
     const dispatch = useDispatch();
@@ -15,7 +16,10 @@ const Chooser = () => {
         <Modal>
             <Modal.Header onClose={close}>Choose</Modal.Header>
             <Modal.Body>
-
+                <div className="flex justify-center space-x-8">
+                    <Button text="Upload a photo" variant="link" />
+                    <Button text="Take a picture" variant="link" />
+                </div>
             </Modal.Body>
         </Modal>
     );
