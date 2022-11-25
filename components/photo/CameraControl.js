0c7cf4d3bca9a2a5581af92f11e6943fdafedbdc
lvidal1 <lvidal910@gmail.com>
2022-11-24 23:25:18 -0500

    Update camera props

--- components/photo/CameraControl.js
+++ components/photo/CameraControl.js
@@ -1,7 +1,8 @@
 import useImage from "@hooks/useImage";
 import { useEffect, useState } from "react";
-import Camera from "react-html5-camera-photo";
+import Camera, { FACING_MODES, IMAGE_TYPES } from "react-html5-camera-photo";
 import "react-html5-camera-photo/build/css/index.css";
+import styles from "@styles/CameraControl.module.css";
 
 const CameraControl = ({ children }) => {
 	const { readImageFromCamera } = useImage();
@@ -12,18 +13,36 @@ const CameraControl = ({ children }) => {
 	}, []);
 
 	const handleTakePhotoAnimationDone = (dataUri) => {
-		readImageFromCamera(dataUri);
 		setOpenCamera(false);
 	};
+
+	const handleTakePhoto = (dataUri) => {
+		readImageFromCamera(dataUri);
+	};
+
+	const [fullScreen, setFullScreen] = useState(false);
 
 	return (
 		<>
 			<div onClick={() => setOpenCamera(true)}>{children}</div>
 			{openCamera && (
-				<Camera
-					onTakePhotoAnimationDone={handleTakePhotoAnimationDone}
-					isFullscreen={false}
-				/>
+				<div className="fixed w-full h-full top-0 left-0 bg-black flex items-center justify-center">
+					<div className={styles.cameraControl}>
+						<Camera
+							onTakePhoto={handleTakePhoto}
+							idealFacingMode={FACING_MODES.ENVIRONMENT}
+							onTakePhotoAnimationDone={
+								handleTakePhotoAnimationDone
+							}
+							isFullscreen={fullScreen}
+							idealResolution={
+								fullScreen ? { width: 360, height: 740 } : {}
+							}
+							imageCompression={0.97}
+							sizeFactor={1}
+						/>
+					</div>
+				</div>
 			)}
 		</>
 	);
