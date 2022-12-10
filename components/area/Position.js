efcecfb1e1f44dcb57e2fe4fd0e4d00cfd2b9378
Leonardo Vidal <lvidal910@gmail.com>
2022-12-10 01:31:06 -0500

    Update position on area (#22)

---
+++ components/area/Position.js
@@ -0,0 +1,22 @@
+import Image from "next/image";
+
+const Position = ({ position }) => {
+	return (
+		<div className="bg-white w-12 h-14 relative">
+			<div className="w-full h-full">
+				<Image
+					src="/soccer-icon.png"
+					alt="Soccer field"
+					layout="fill"
+					objectFit="cover"
+					quality={100}
+				/>
+			</div>
+			<div className="absolute z-0 w-full top-0 flex font-bold justify-center z-10">
+				{position}
+			</div>
+		</div>
+	);
+};
+
+export default Position;
